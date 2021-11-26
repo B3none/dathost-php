c8f4c86ddd239998f5051796f4bfeb9d43d4e77e
Alex Blackham <alexblackham@bluelightservice.co.uk>
2021-11-26 10:15:18 +0000

    Added createServer endpoint

--- src/Categories/ServerCategory.php
+++ src/Categories/ServerCategory.php
@@ -4,5 +4,20 @@ namespace B3none\DatHost\Categories;
 
 class ServerCategory extends BaseCategory
 {
+	/**
+	 * Create server
+	 *
+	 * @param array $serverDetails
+	 * @return array
+	 */
+	public function createServer(array $serverDetails): array
+	{
+		$response = $this->guzzle->post('/game-servers', [
+			'body' => $serverDetails,
+		]);
 
+		$responseContents = $response->getBody()->getContents();
+
+		return json_decode($responseContents, true);
+	}
 }
