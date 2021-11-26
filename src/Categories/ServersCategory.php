b4ec4053dd8ead5fb98d5cff0e022e8e845a5113
Alex Blackham <alexblackham@bluelightservice.co.uk>
2021-11-26 10:15:37 +0000

    added getServers endpoint

--- src/Categories/ServersCategory.php
+++ src/Categories/ServersCategory.php
@@ -4,5 +4,17 @@ namespace B3none\DatHost\Categories;
 
 class ServersCategory extends BaseCategory
 {
+	/**
+	 * Get a list of non-deleted game servers
+	 *
+	 * @return array
+	 */
+	public function getServers(): array
+	{
+		$response = $this->guzzle->get('/game-servers');
 
+		$responseContents = $response->getBody()->getContents();
+
+		return json_decode($responseContents, true);
+	}
 }
