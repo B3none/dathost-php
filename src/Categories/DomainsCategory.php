06e21b76c6a1a8496a98d8e52b37eac515aefb7c
Alex Blackham <alexblackham@bluelightservice.co.uk>
2021-11-26 09:57:14 +0000

    Add getCustomDomains function

--- src/Categories/DomainsCategory.php
+++ src/Categories/DomainsCategory.php
@@ -4,5 +4,17 @@ namespace B3none\DatHost\Categories;
 
 class DomainsCategory extends BaseCategory
 {
+	/**
+	 * Get custom domains
+	 *
+	 * @return array
+	 */
+	public function getCustomDomains(): array
+	{
+		$response = $this->guzzle->get('/custom-domains');
 
+		$responseContents = $response->getBody()->getContents();
+
+		return json_decode($responseContents, true);
+	}
 }
