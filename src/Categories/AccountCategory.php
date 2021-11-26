35b9188b3d916a010527988671e42153cb8abe3e
Alex Blackham <alexblackham@bluelightservice.co.uk>
2021-11-26 09:55:07 +0000

    Add getAccount endpoint

--- src/Categories/AccountCategory.php
+++ src/Categories/AccountCategory.php
@@ -4,8 +4,17 @@ namespace B3none\DatHost\Categories;
 
 class AccountCategory extends BaseCategory
 {
-    public function getAccount()
+	/**
+	 * Get info about the account
+	 *
+	 * @return array
+	 */
+    public function getAccount(): array
     {
+		$response = $this->guzzle->get('/account');
 
+		$responseContents = $response->getBody()->getContents();
+
+		return json_decode($responseContents, true);
     }
 }
